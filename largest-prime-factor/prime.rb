41de65304361a3cded481a6f8c6eb47994fa02e0
randallreedjr <randallreedjr@gmail.com>
2014-08-15 23:08:28 -0400

    Refactor into class. Use memoization of primes.

--- largest-prime-factor/prime.rb
+++ largest-prime-factor/prime.rb
@@ -1,23 +1,35 @@
-def largest_prime_factor(number)
-  limit = number/3
-  i = 3
-  while i < limit
-    if number % i == 0 && prime?(number/i)
-      return number/i
+class Prime
+  def initialize(number)
+    @number = number
+    @primes = {}
+  end
+
+  def largest_prime_factor()
+    return @number if @number == 2
+    (3..@number/3).step(2) do |divisor|
+      return @number/divisor if prime_factor?(divisor)
     end
-    i += 2
+  end
+
+  private
+  def prime_factor?(divisor)
+    if @number % divisor == 0
+      @primes[@number/divisor] ||= prime?(@number/divisor)
+    end
+  end
+
+  def prime?(number)
+    if number < 2
+      return false
+    elsif number > 2
+      (2..number ** 0.5).each do |i|
+        return false if number % i == 0
+      end
+    end
+    return true
   end
 end
-
-def prime?(number)
-  return false if number < 2
-  return true if number == 2
-
-  limit = number ** 0.5
-  (2..limit).each do |i|
-    return false if number % i == 0
-  end
-  return true
-end
-
-puts largest_prime_factor(600851475143)
+puts Time.now
+factor = Prime.new(600851475143)
+puts factor.largest_prime_factor
+puts Time.now
