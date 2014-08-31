dc8eceaffd0bedc08bdccbd4ca25a0b48f01b40b
randallreedjr <randallreedjr@gmail.com>
2014-08-31 17:48:08 -0400

    Solve Project Euler Problem 1

---
+++ 001-multiples/multiples.rb
@@ -0,0 +1,38 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+class Multiples
+  attr_reader :loops
+  def initialize(multiples)
+    @multiples = multiples.sort
+    @sum = 0
+  end
+
+  #More iterations but clearer code
+  def sum(limit)
+    @sum = (1...limit).select { |term| multiple?(term) }.inject(:+)
+  end
+
+  #Fewer iterations but more obscure code
+  def sum2(limit)
+    @sum = 0
+    @multiples.each_with_index do |multiple_to_sum, index|
+      (0...limit).step(multiple_to_sum) do |term|
+        @loops += 1
+        @sum += term unless term % @multiples[0..index].inject(:*) == 0 && index > 0
+      end
+    end
+    @sum
+  end
+
+  private
+  def multiple?(term)
+    @multiples.each do |multiple_to_check|
+      return true if term % multiple_to_check == 0
+    end
+    return false
+  end
+end
+
+test = Multiples.new([3,5])
+puts "#{test.sum(1000)} in #{test.loops} loops"
+puts "#{test.sum2(1000)} in #{test.loops} loops"
