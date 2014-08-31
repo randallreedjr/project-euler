335c853c2e1df22721ed10720ce41afabbb1fb0e
randallreedjr <randallreedjr@gmail.com>
2014-08-31 17:50:43 -0400

    Remove references to loops variable

--- 001-multiples/multiples.rb
+++ 001-multiples/multiples.rb
@@ -1,7 +1,6 @@
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 class Multiples
-  attr_reader :loops
   def initialize(multiples)
     @multiples = multiples.sort
     @sum = 0
@@ -17,7 +16,6 @@ class Multiples
     @sum = 0
     @multiples.each_with_index do |multiple_to_sum, index|
       (0...limit).step(multiple_to_sum) do |term|
-        @loops += 1
         @sum += term unless term % @multiples[0..index].inject(:*) == 0 && index > 0
       end
     end
@@ -34,5 +32,5 @@ class Multiples
 end
 
 test = Multiples.new([3,5])
-puts "#{test.sum(1000)} in #{test.loops} loops"
-puts "#{test.sum2(1000)} in #{test.loops} loops"
+puts "#{test.sum(1000)} with first method"
+puts "#{test.sum2(1000)} with second method"
